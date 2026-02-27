3ef4b743f6a261addefc8e88786ae8518ecc3f72
Sulara Perera <sulara.perera@outlook.com>
2026-02-27 06:54:38 +0000

    hid secret in env variable

--- macros/setup_env.sql
+++ macros/setup_env.sql
@@ -1,9 +1,10 @@
 {% macro setup_environment(
     warehouse_name,
     database_name,
-    git_username='sularaperera', 
-    git_token='eyJraWQiOiI4MjcwODE4NTIyODkwMzAiLCJhbGciOiJFUzI1NiJ9.eyJwIjoiNDkyOTc5MjQ6NDkyOTc5MjQiLCJpc3MiOiJTRjoyMDAzIiwiZXhwIjoxNzc0NjM5MTM5fQ.0DiB-12FCbT7XrxeD0IVETVF8O1KJkUo9UDjNYoV9rlDcMe4YqUtTMj584g8CevtblWaaAox0_GRQ3BLJlHaJw')
+    git_username='sularaperera')
 %}
+
+{% set git_token = env_var('DBT_GIT_TOKEN') %}
 
 -- =========================
 -- Warehouse
